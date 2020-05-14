8db55e3d86df4af26c06a84911faaeeac9da386a
Dmytro Kuschevyy <scaring22@gmail.com>
2020-05-14 18:39:47 +0300

    Updates

---
+++ precache-manifest.66e212c971ab3b91bfa9e6c384cb0b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcbb239e28ca95e09f662f114e9f14c",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "4adcccced5dc0a5b1fbe",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.65cdd653.chunk.css"
+  },
+  {
+    "revision": "c4bd5aa8fe070f3dba87",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.091674d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.091674d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4adcccced5dc0a5b1fbe",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.21ee80b5.chunk.js"
+  },
+  {
+    "revision": "2c33b5407f6ddb0305a4",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.395ab540.js"
+  }
+]);
